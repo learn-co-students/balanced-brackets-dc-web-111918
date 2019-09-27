3d20662a17ac8bef1935a719948f81ebff6c480c
Rob Cobb <rwcobbjr@gmail.com>
2019-09-27 10:44:19 -0400

    add additional test cases

--- test/index-test.js
+++ test/index-test.js
@@ -1,18 +1,39 @@
 describe('#isBalanced', function() {
-	it('returns true if string is balanced', function(){
+  it('returns true for simple balanced parens', function() {
+    expect(isBalanced('()')).toEqual(true);
+  })
+
+  it('returns true for multiple balanced parens', function() {
+    expect(isBalanced("()[]")).toEqual(true)
+  })
+
+  it('returns true for nested balanced parens and brackets', function() {
+    expect(isBalanced("([])")).toEqual(true);
+  });
+
+
+  it('returns false for simple unbalanced parens', function() {
+    expect(isBalanced("(")).toEqual(false);
+  })
+
+  it('returns false for unbalanced parens inside of balanced parens', function() {
+    expect(isBalanced("{[}")).toEqual(false);
+  })
+
+  it('returns false for overlapping but otherwise balanced parens', function() {
+    expect(isBalanced("({)}")).toEqual(false);
+  });
+
+	it('returns true for complex balanced strings', function(){
 		let str = '([])[{}]{(())}'
 		expect(isBalanced(str)).toEqual(true)
 	})
-});
 
-describe('#isBalanced', function() {
 	it('returns false if string is not balanced', function(){
 		let str = '([])[{}]{(([))}'
 		expect(isBalanced(str)).toEqual(false)
 	})
-});
 
-describe('#isBalanced', function() {
 	it('returns false even if string has all brackets matched', function(){
 		let str = '([])[{}]{([)]}'
 		expect(isBalanced(str)).toEqual(false)
